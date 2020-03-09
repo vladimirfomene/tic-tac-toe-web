74cdad91309fd611cfe73a837b9f5a453add2c2b
vladimirfomene <vladimirfomene@gmail.com>
2020-03-09 20:08:36 +0100

    add database config file

---
+++ backend/knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: "mysql",
+    connection: {
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE_NAME
+    }
+  }
+
+};
