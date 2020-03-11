5240e74004d1e55cbd56b00f81c39690052fe1a1
vladimirfomene <vladimirfomene@gmail.com>
2020-03-11 15:38:48 +0100

    add cors support, body-parser for serializing request body

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const router = express.Router();
 const port = 3000;
 
 const winnerController = require("./controllers/winners");
 const aiController = require("./controllers/ai");
 
+app.use(cors({ origin: "http://localhost:8080" }));
+app.use(bodyParser.json());
+
 router.route("/winners")
     .get(winnerController.getWinners)
     .post(winnerController.createWinner);
 
 router.route("/ai")
-    .get(aiController.findBestMove);
+    .post(aiController.getAIMove);
 
 // Mount router
 app.use("/", router);
