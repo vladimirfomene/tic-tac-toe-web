5cd404f39551a794c17d9ca78e195e162ce876b7
vladimirfomene <vladimirfomene@gmail.com>
2020-03-12 19:23:47 +0100

    create footer component

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    Made with <span class="heart-icon">&hearts;</span> in Cameroon
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style scoped>
+.heart-icon {
+  font-size: 100%;
+  color: red;
+}
+
+footer {
+  font-family: "Oswald", sans-serif;
+  font-size: 100%;
+  font-weight: bold;
+  margin-top: 10px;
+  justify-self: center;
+  align-self: end;
+}
+</style>
