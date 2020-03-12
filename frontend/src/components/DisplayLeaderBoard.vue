698fe9d8c66d86ab1f6b23a12790af7df10dcca5
vladimirfomene <vladimirfomene@gmail.com>
2020-03-12 19:30:14 +0100

    add button to display leaderboard

---
+++ frontend/src/components/DisplayLeaderBoard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="leader-container">
+    <button class="btn-leader" @click="displayLeaderBoard">
+      Display LeaderBoard
+    </button>
+  </div>
+</template>
+
+<script>
+import EventBus from "../event_bus";
+
+export default {
+  name: "DisplayLeaderBoard",
+  methods: {
+    displayLeaderBoard() {
+      EventBus.$emit("display-leaderboard");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.btn-leader {
+  background-color: #fbbc05;
+  border-radius: 5px;
+  border: 0;
+  color: #fff;
+  cursor: pointer;
+  font-weight: bold;
+  float: left;
+  padding: 8px 16px;
+  justify-self: center;
+}
+
+.leader-container {
+  margin-top: 3%;
+  justify-self: center;
+}
+</style>
